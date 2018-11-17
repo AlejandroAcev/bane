509c12ade4d16fb552a52368973db53cb9a7de85
Alejandro Acevedo Gonzalez <alej.acev.gonz@gmail.com>
2018-11-17 13:37:23 +0100

    Creados metodos de control de movimiento

--- Movement.py
+++ Movement.py
@@ -1,19 +1,46 @@
 import RPi.GPIO as gpio
-
+import time
 
 #Pins - Motor's controller
-motorA_pin_forward = 10
-motorB_pin_forward = 11
-motorA_pin_backward = 12
-motorB_pin_backward = 13
+#Marron
+motorA_pin_forward = 4
+#Rojo
+motorB_pin_forward = 17
+#Naranja
+motorA_pin_backward = 27
+#verde
+motorB_pin_backward = 22
 
 
 #Setting up pins
-def init()
+def init():
 	gpio.setmode(gpio.BOARD)
 	
+	gpio.setup(motorA_pin_forward, gpio.OUT)
+	gpio.setup(motorB_pin_forward, gpio.OUT)
+	gpio.setup(motorA_pin_backward, gpio.OUT)
+	gpio.setup(motorB_pin_backward, gpio.OUT)
 
-	gpio.setup(7, gpio.OUT)
-	gpio.setup(11, gpio.OUT)
-	gpio.setup(13, gpio.OUT)
-	gpio.setup(15, gpio.OUT)
+
+def forward():
+	init()
+	gpio.output(motorA_pin_forward, True)
+	gpio.output(motorB_pin_forward, True)
+	gpio.output(motorA_pin_backward, False)
+	gpio.output(motorB_pin_backward, False)
+	time.sleep(3)
+	
+
+def reverse():
+	init()
+	gpio.output(motorA_pin_forward, False)
+	gpio.output(motorB_pin_forward, False)
+	gpio.output(motorA_pin_backward, True)
+	gpio.output(motorB_pin_backward, True)
+	time.sleep(3)
+
+print("Hacia adelante")
+forward()
+time.sleep(2)
+print("Hacia atras")
+reverse()
